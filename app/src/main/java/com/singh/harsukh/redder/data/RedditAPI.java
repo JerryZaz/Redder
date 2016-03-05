9564c92aebc5b713adc90bb2607d722652f7c858
Henry <me@hnry.us>
2016-03-04 22:54:21 -0500

    Gave the endpoint an appropriate name and wrote a couple a unit test for it

--- app/src/main/java/com/singh/harsukh/redder/data/RedditAPI.java
+++ app/src/main/java/com/singh/harsukh/redder/data/RedditAPI.java
@@ -10,8 +10,9 @@ import retrofit2.http.Path;
  * Created by Henry on 3/4/2016.
  */
 public interface RedditAPI {
+
     @GET("r/{subreddit}/.json")
-    Call<Listing> getData(
+    Call<Listing> getPostsFromSubreddit(
             @Path("subreddit") String subreddit
     );
 }
