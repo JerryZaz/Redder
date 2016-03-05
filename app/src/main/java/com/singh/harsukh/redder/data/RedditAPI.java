6d626e99ce2377d9cb320d1f2e44e461ecb1535b
Henry <me@hnry.us>
2016-03-04 21:31:38 -0500

    First endpoint and model class ready, takes a subreddit as parameter.

---
+++ app/src/main/java/com/singh/harsukh/redder/data/RedditAPI.java
@@ -0,0 +1,17 @@
+package com.singh.harsukh.redder.data;
+
+import com.singh.harsukh.redder.model.Listing;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by Henry on 3/4/2016.
+ */
+public interface RedditAPI {
+    @GET("r/{subreddit}/.json")
+    Call<Listing> getData(
+            @Path("subreddit") String subreddit
+    );
+}
