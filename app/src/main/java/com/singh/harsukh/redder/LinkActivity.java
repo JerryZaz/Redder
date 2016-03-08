f5241c75f05729f245db78eae84569fd94ef4a05
Henry <me@hnry.us>
2016-03-08 12:31:19 -0500

    Set up for LinkActivity

---
+++ app/src/main/java/com/singh/harsukh/redder/LinkActivity.java
@@ -0,0 +1,13 @@
+package com.singh.harsukh.redder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_link);
+    }
+}
